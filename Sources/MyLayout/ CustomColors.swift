b791b9ba381a8214ddb78bdfc63a84277673bd48
Leopold Lemmermann <leopold@lemmermann.com>
2021-12-14 23:53:40 +0100

    removed primary color redeclaration

--- Sources/MyLayout/ CustomColors.swift
+++ Sources/MyLayout/ CustomColors.swift
@@ -9,8 +9,6 @@ import SwiftUI
 
 ///MARK: custom colors
 extension Color {
-    public static let primary = Color("primary"),
-                      secondary = Color("secondary"),
-                      primaryAlpha = Color.primary.opacity(LayoutDefaults().opacity),
+    public static let primaryAlpha = Color.primary.opacity(LayoutDefaults().opacity),
                       secondaryAlpha = Color.secondary.opacity(LayoutDefaults().opacity)
 }
