9a6a847687b465c1464d7bbd01ac7364d33f6301
Leopold Lemmermann <leopold@lemmermann.com>
2021-12-30 21:21:31 +0100

    changed the version requirement

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "MyPackage",
     platforms: [
-        .iOS(.v15)
+        .iOS(.v14)
     ],
     products: [
         .library(name: "MyPackage",
